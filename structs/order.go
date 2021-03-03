a99b9946e7315d271bc1ba2c7868002939aee44e
Hank Shen <swhbox@foxmail.com>
2021-03-03 16:11:41 +0800

    update

--- structs/order.go
+++ structs/order.go
@@ -8,28 +8,18 @@ import (
 )
 
 type Order struct {
-	OrderID         string  `json:"order_id"`
-	UserID          int64   `json:"user_id"`
 	MerchantOrderID string  `json:"merchant_order_id"`
+	PriceAmount     float64 `json:"price_amount"`
+	PriceCurrency   string  `json:"price_currency"`
+	PayCurrency     string  `json:"pay_currency"`
 	Title           string  `json:"title"`
 	Description     string  `json:"description"`
 	CallbackURL     string  `json:"callback_url"`
 	CancelURL       string  `json:"cancel_url"`
 	SuccessURL      string  `json:"success_url"`
-	PriceAmount     float64 `json:"price_amount"`
-	PriceCurrency   string  `json:"price_currency"`
-	Status          string  `json:"status"`
-	Notified        string  `json:"notified"`
-	CreatedAt       string  `json:"created_at"`
-	UpdatedAt       string  `json:"updated_at"`
-	PayAmount       float64 `json:"pay_amount"`
-	PayCurrency     string  `json:"pay_currency"`
-	IsSelf          bool    `json:"is_self"`
 	Mobile          bool    `json:"mobile"`
 	Fast            bool    `json:"fast"`
 	Token           string  `json:"token"`
-	PaidAt          string  `json:"paid_at"`
-	ReceiveCurrency string  `json:"receive_currency"`
 }
 
 // Sign 签名
--- structs/order_server.go
+++ structs/order_server.go
@@ -4,13 +4,38 @@ import (
 	"fmt"
 )
 
+type ResponseOrder struct {
+	OrderID         string  `json:"order_id"`
+	UserID          int64   `json:"user_id"`
+	MerchantOrderID string  `json:"merchant_order_id"`
+	Title           string  `json:"title"`
+	Description     string  `json:"description"`
+	CallbackURL     string  `json:"callback_url"`
+	CancelURL       string  `json:"cancel_url"`
+	SuccessURL      string  `json:"success_url"`
+	PriceAmount     float64 `json:"price_amount"`
+	PriceCurrency   string  `json:"price_currency"`
+	Status          string  `json:"status"`
+	Notified        string  `json:"notified"`
+	CreatedAt       string  `json:"created_at"`
+	UpdatedAt       string  `json:"updated_at"`
+	PayAmount       float64 `json:"pay_amount"`
+	PayCurrency     string  `json:"pay_currency"`
+	IsSelf          bool    `json:"is_self"`
+	Mobile          bool    `json:"mobile"`
+	Fast            bool    `json:"fast"`
+	Token           string  `json:"token"`
+	PaidAt          string  `json:"paid_at"`
+	ReceiveCurrency string  `json:"receive_currency"`
+}
+
 type ServerOrder struct {
-	Status     int      `json:"status"`
-	Order      Order    `json:"order"`
-	Merchant   Merchant `json:"merchant"`
-	PaymentURL string   `json:"payment_url"`
-	Invoice    Invoice  `json:"invoice"`
-	Permission string   `json:"permission"`
+	Status     int           `json:"status"`
+	Order      ResponseOrder `json:"order"`
+	Merchant   Merchant      `json:"merchant"`
+	PaymentURL string        `json:"payment_url"`
+	Invoice    Invoice       `json:"invoice"`
+	Permission string        `json:"permission"`
 
 	// Failed:
 	// MugglePay Server will always return status 400. If API failed, it will return error_code and error as its object.
