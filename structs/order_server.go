97b5d267fdae47e7761399870e70e7c1f6fc618b
Hank Shen <swhbox@foxmail.com>
2021-03-03 11:34:54 +0800

    update

--- structs/order_server.go
+++ structs/order_server.go
@@ -2,8 +2,6 @@ package structs
 
 import (
 	"fmt"
-
-	"github.com/admpub/mugglepay/utils"
 )
 
 type ServerOrder struct {
@@ -22,16 +20,6 @@ type ServerOrder struct {
 
 // QRCodeURLParsers 二维码网址解析器
 var QRCodeURLParsers = map[string]func(*ServerOrder){
-	`ALIPAY`: func(s *ServerOrder) {
-		if rurl := utils.URLQueryValueGetter(s.Invoice.Qrcode)("url"); len(rurl) > 0 {
-			s.Invoice.Address = rurl
-		} else {
-			s.Invoice.Address = utils.URLQueryValueGetter(s.Invoice.QrcodeLg)("mpurl")
-		}
-	},
-	`WECHAT`: func(s *ServerOrder) {
-		s.Invoice.Address = s.Invoice.Qrcode
-	},
 	`EOS`: func(s *ServerOrder) {
 		s.Invoice.Address = "mgtestflight"
 		s.Invoice.Memo = fmt.Sprintf("MP:%s", s.Invoice.OrderID)
